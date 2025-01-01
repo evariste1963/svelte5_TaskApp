8103c87e5b1bc50776b26aafc00f74c5b206e41c
evariste1963 <evariste@hotmail.co.uk>
2025-01-01 08:30:14 +0000

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+	import TasksForm from '../components/tasks-form.svelte';
+
+	type Task = {
+		id: string;
+		title: string;
+		done: boolean;
+	};
+
+	let message = 'Tasks App';
+	let tasks = $state<Task[]>([]);
+
+	function addTask(newTask: string) {
+		tasks.push({
+			id: crypto.randomUUID(),
+			title: newTask,
+			done: false
+		});
+	}
+</script>
+
+<main>
+	<h1>{message}</h1>
+	<TasksForm {addTask} />
+	<section>
+		{#each tasks as task}
+			<article>
+				{task.title}
+			</article>
+		{/each}
+	</section>
+</main>
+
+<style>
+	main {
+		margin: 1rem auto;
+		max-width: 800px;
+	}
+</style>
